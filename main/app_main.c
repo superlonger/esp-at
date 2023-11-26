310048644cfc372108370529e6ac6dcf8871e703
superlonger <superlonger@gmail.com>
2023-11-26 20:52:03 +0800

    add LED control

--- main/app_main.c
+++ main/app_main.c
@@ -41,6 +41,21 @@
 #include "esp_at.h"
 #include "at_interface.h"
 
+#define GPIO_OUTPUT_IO_0    21
+#define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
+
+static void gpio_task_example(void* arg)
+{
+    for(;;) {
+        //printf("turn off led0!\r\n");
+        gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        //printf("turn on led0!\r\n");
+        gpio_set_level(GPIO_OUTPUT_IO_0, 1);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
+
 #ifndef CONFIG_AT_SOCKET_MAX_CONN_NUM
 #define CONFIG_AT_SOCKET_MAX_CONN_NUM       1
 #endif
@@ -269,6 +284,22 @@ void app_main(void)
         printf("regist rainmaker cmd fail\r\n");
     }
 #endif
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
 
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    
     at_custom_init();
 }
